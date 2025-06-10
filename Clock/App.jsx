f2dd94b8511f93ac9837a9ad60d3ff7a9bbb33ef
Ananya Goel <133999243+Ananya142@users.noreply.github.com>
2025-06-10 18:34:02 +0530

    Create App.jsx

---
+++ Clock/App.jsx
@@ -0,0 +1,31 @@
+
+function App() {
+  let count = 0; 
+  let [cnt, setcnt] = useState(0);
+  let date_time = new Date();
+  date_time.setDate(date_time.getDate() + cnt);
+
+  return (
+    <>
+      <button onClick={() => {
+        console.log(count++);
+        setcnt(cnt+1);
+      }}>
+        plus+
+      </button>
+      
+      <p> count={cnt}</p> 
+      
+      <button onClick={() => {
+        console.log(count--);
+        setcnt(cnt - 1);
+      }}>
+        minus-
+      </button>
+
+      <div>
+        <p>{date_time.toDateString()}</p>
+      </div>
+    </>
+  );
+}
